30f3947d422c756e98e01499d31fe4610f4c8a39
knish502 <40375387+knish502@users.noreply.github.com>
2025-03-01 13:38:51 -0600

    Update Guesser.kt

--- guesser/src/main/kotlin/Guesser.kt
+++ guesser/src/main/kotlin/Guesser.kt
@@ -1,11 +1,24 @@
 package org.example
+import kotlin.math.log2
+import kotlin.math.floor
 
 class Guesser {
+    var keepRunning = true
+    var guesses = emptyArray<Int>()
+    var ans = 0
+    var min = 0
+    var max = 1000
+    var guessCount = floor(log2(this.max.toDouble())).toInt() + 1
+
     init {
         // pass
     }
 
     fun run() {
-        println("got here! yeet")
+        var menuOption = 0
+        var status = 0
+        while (this.keepRunning) {
+            /* ... */
+        }
     }
 }
