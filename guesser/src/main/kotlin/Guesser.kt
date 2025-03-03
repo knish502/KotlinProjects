d3687ca535f0fc54feee9706352ad7a9d542f409
knish502 <40375387+knish502@users.noreply.github.com>
2025-03-02 22:26:29 -0600

    Update Guesser.kt

--- guesser/src/main/kotlin/Guesser.kt
+++ guesser/src/main/kotlin/Guesser.kt
@@ -10,23 +10,56 @@ class Guesser {
     var max = 1000
     var guessCount = floor(log2(this.max.toDouble())).toInt() + 1
 
+    enum class MenuType {
+        eTopMenu,
+        eParametersMenu
+    }
+
     init {
         // pass
     }
 
     fun run() {
         var menuOption = 0
+        var parameterMenuOption = 0
         var status = 0
         while (this.keepRunning) {
-            // get menu input
-                // check if need to break
-            // make changes
+            menuOption = this.menu(MenuType.eTopMenu)
+            when (menuOption) {
+                0 -> break
+                2 -> parameterMenuOption = this.menu(MenuType.eParametersMenu)
+            }
             if (! this.turns()) { break }
         }
+    }
+
+    fun menu(menuType: MenuType) : Int {
+        val topMenuText = "Choose an option:\n" +
+                "\t0 - Exit program\n" +
+                "\t1 - New Game\n" +
+                "\t2 - Change game parameters\n" +
+                "> "
+
+        val adjustGameMenu = "Choose an option:\n" +
+                "\t0 - Return to top menu\n" +
+                "\t1 - Change minimum guess\n" +
+                "\t2 - Change maximum guess\n" +
+                "\t3 - Change number of guesses\n" +
+                "> "
+
+        when (menuType) {
+            MenuType.eTopMenu -> print(topMenuText)
+            MenuType.eParametersMenu -> print(adjustGameMenu)
+        }
+
+        val inputValue = readLine()?.toInt() ?: 0
+        return inputValue
     }
 
     fun turns() : Boolean {
         println("taking a turn!")
         return false
     }
+
+
 }
