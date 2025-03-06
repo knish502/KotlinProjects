b533d782e4bbf2ac6baf7d0b5c05e483f927991f
Kyle Nishimuta <40375387+knish502@users.noreply.github.com>
2025-03-06 16:15:22 -0600

    Create main.kt

---
+++ test/main.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+
+    /*
+    Goal:
+        i will continue to increase
+        d resets to 0 at some point along the line
+        i and d should increase at the same rate, including after d is reset
+
+        ex:
+        i   d
+        --+--
+        0   0
+        1   1
+        2   2
+        3   0  <- d resets here
+        4   1
+        5   2
+        6   3
+        7   4
+     */
+
+    var d = 0
+    for (i in 0..25) {
+        d = i
+        if (i == 8) {
+            d = 0
+        }
+        println("${i} -> ${d}")
+    }
+}
