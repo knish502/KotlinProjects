290989ba9486abd6fd56321e1323649252f57f5c
knish502 <40375387+knish502@users.noreply.github.com>
2025-03-06 21:04:26 -0600

    Update main.kt

--- test/main.kt
+++ test/main.kt
@@ -5,7 +5,8 @@ fun main(args: Array<String>) {
         i will continue to increase
         d resets to 0 at some point along the line
         i and d should increase at the same rate, including after d is reset
-
+		the rate of increase is unknown
+        
         ex:
         i   d
         --+--
@@ -20,10 +21,12 @@ fun main(args: Array<String>) {
      */
 
     var d = 0
+    var diff = 0
     for (i in 0..25) {
-        d = i
+        d = i + diff
         if (i == 8) {
             d = 0
+            diff = -i
         }
         println("${i} -> ${d}")
     }
