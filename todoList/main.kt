1146178f957fa95b05e0b6bc2707f28c6f13025e
Kyle Nishimuta <40375387+knish502@users.noreply.github.com>
2025-03-07 14:24:34 -0600

    update todoList/main.kt

--- todoList/main.kt
+++ todoList/main.kt
@@ -2,23 +2,21 @@ import java.io.File
 
 fun main() {
     val filename = "list.txt"
-    val contentLines: List<String> = File(filename).readLines()
-    var completionMap: MutableMap<String, Boolean> = mutableMapOf()
-    var orderedKeyList: MutableList<String> = mutableListOf()
+    var dataStore = DataStruct(filename)
     
     while (true) {
         when (menu()) {
-            0 -> viewList()
-            1 -> checkOffItem()
-            2 -> addItem()
-            3 -> removeItem()
+            0 -> dataStore.viewList()
+            1 -> dataStore.checkOffItem()
+            2 -> dataStore.addItem()
+            3 -> dataStore.removeItem()
             4 -> break
             else -> println("That's not an option, please try again")
         }
     }
 
-    saveData(completionMap, orderedKeyList, filename)
-    do {println("Press 'q' to exit:")} while (readLine() != "q")
+    dataStore.saveData()
+    do {println("Press 'q' to confirm:")} while (readLine() != "q")
 }
 
 fun menu() : Int {
@@ -28,18 +26,62 @@ fun menu() : Int {
                 " 2: Add item to to-do list\n" +
                 " 3: Remove item from to-do list\n" + 
                 " 4: Quit the program\n> "
-    println(menuText)
+    print(menuText)
     return readLine()?.toInt() ?: -1
 }
 
-fun viewList() {}
 
-fun checkOffItem() {}
 
-fun addItem() {}
+class DataStruct (val fileName: String) {
 
-fun removeItem() {}
+    var completionMap: MutableMap<String, Boolean> = mutableMapOf<String, Boolean>()
+    var keyList: MutableList<String> = mutableListOf()
 
-fun saveData(dataMap: MutableMap<String, Boolean>, 
-             keyList: MutableList<String>, 
-             fileName: String) {}
+    init {
+        this.loadData()
+        this.keyList = this.completionMap.keys.toMutableList()
+    }
+
+    fun loadData() {
+        val dataFileLines : List<String> = File(fileName).readLines()
+        this.completionMap = mutableMapOf<String, Boolean>()
+        var key = ""
+        var value = false
+        for (line in dataFileLines) {
+            value = false
+            if (line.take(1) == "*") {
+                key = line.substring(1)
+                value = true
+            }
+            else {
+                key = line
+            }
+            this.completionMap[key] = value
+        }
+    }
+    
+    fun viewList() {
+        println("\nDisplaying your to-do list:\n")
+        for (i in 0..this.keyList.size-1){
+            print("$i.")
+            if (this.completionMap[this.keyList[i]] ?: false){
+                print(" [complete]")
+            }
+            print(" ${this.keyList[i]}\n")
+        }
+    }
+    
+    fun checkOffItem() {
+        print("\nEnter the index of the item you wish to check off:\n> ")
+        val idx = readLine()?.toInt() ?: -100
+        this.completionMap[this.keyList[idx]] = true
+        println("You have checked off '${this.keyList[idx]}'")
+    }
+    
+    fun addItem() {}
+    
+    fun removeItem() {}
+    
+    fun saveData() {}
+
+}
