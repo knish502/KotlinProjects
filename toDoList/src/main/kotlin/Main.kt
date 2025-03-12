8dc5b6a4b5c4dc82c28a921fef62d6b803e231d4
knish502 <40375387+knish502@users.noreply.github.com>
2025-03-11 20:08:26 -0500

    refactor toDoList

--- toDoList/src/main/kotlin/Main.kt
+++ toDoList/src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import java.io.File
 fun main() {
     val filename = "list.txt"
     var dataStore = DataStruct(filename)
-    
+
     while (true) {
         when (menu()) {
             0 -> dataStore.viewList()
@@ -16,26 +16,26 @@ fun main() {
     }
 
     dataStore.saveData()
-    do {println("Press 'q' to confirm:")} while (readLine() != "q")
+    do {println("Press 'q' to confirm:")} while (readlnOrNull() != "q")
 }
 
 fun menu() : Int {
     val menuText = "\nEnter an option:\n" +
-                " 0: View to-do list\n" +
-                " 1: Mark item as completed\n" +
-                " 2: Add item to to-do list\n" +
-                " 3: Remove item from to-do list\n" + 
-                " 4: Quit the program\n> "
+            " 0: View to-do list\n" +
+            " 1: Mark item as completed/incomplete\n" +
+            " 2: Add item to to-do list\n" +
+            " 3: Remove item from to-do list\n" +
+            " 4: Quit the program\n> "
     print(menuText)
     return readLine()?.toInt() ?: -1
 }
 
 
 
-class DataStruct (val fileName: String) {
+class DataStruct (private val fileName: String) {
 
-    var completionMap: MutableMap<String, Boolean> = mutableMapOf<String, Boolean>()
-    var keyList: MutableList<String> = mutableListOf()
+    private var completionMap: MutableMap<String, Boolean> = mutableMapOf<String, Boolean>()
+    private var keyList: MutableList<String> = mutableListOf()
 
     init {
         this.loadData()
@@ -59,29 +59,40 @@ class DataStruct (val fileName: String) {
             this.completionMap[key] = value
         }
     }
-    
+
     fun viewList() {
         println("\nDisplaying your to-do list:\n")
-        for (i in 0..this.keyList.size-1){
+        for (i in 0..<this.keyList.size){
             print("$i.")
-            if (this.completionMap[this.keyList[i]] ?: false){
+            if (this.completionMap[this.keyList[i]] == true){
                 print(" [complete]")
             }
             print(" ${this.keyList[i]}\n")
         }
     }
-    
+
     fun checkOffItem() {
-        print("\nEnter the index of the item you wish to check off:\n> ")
-        val idx = readLine()?.toInt() ?: -100
-        this.completionMap[this.keyList[idx]] = true
-        println("You have checked off '${this.keyList[idx]}'")
+        print("\nEnter the index of the item you wish to check off or uncheck:\n> ")
+        val idx = readlnOrNull()?.toInt() ?: -100
+        this.completionMap[this.keyList[idx]] = !this.completionMap[this.keyList[idx]]!!
+        println("You have checked '${this.keyList[idx]}'")
     }
-    
-    fun addItem() {}
-    
+
+    fun addItem() {
+        print("\nEnter your task:\n> ")
+        val taskName = readlnOrNull() ?: ""
+        this.completionMap[taskName] = false
+        this.keyList = this.completionMap.keys.toMutableList()
+        var itemIndex = 0
+        for (i in 0..<this.keyList.size){
+            itemIndex = i
+            if (this.keyList[i] == taskName) { break }
+        }
+        println("$taskName has been added at index $itemIndex.")
+    }
+
     fun removeItem() {}
-    
+
     fun saveData() {}
 
 }
