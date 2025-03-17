644497f375a8d020177743ce6929984bdd3806c1
Kyle Nishimuta <40375387+knish502@users.noreply.github.com>
2025-03-17 15:20:27 -0500

    Update Guesser.kt

--- guesser/src/main/kotlin/Guesser.kt
+++ guesser/src/main/kotlin/Guesser.kt
@@ -9,37 +9,64 @@ class Guesser {
     var min = 0
     var max = 1000
     var guessCount = floor(log2(this.max.toDouble())).toInt() + 1
+    var score = 0
 
     init {
         // pass
     }
 
     fun run() {
-        this.ans = (this.min..this.max).random()
-        println("Guess a number between ${this.min} and ${this.max}!")
-        while (this.guesses.size < this.guessCount) {
-            println("You have ${this.guessCount - this.guesses.size} guesses left.")
-            print("Write something:\n> ")
-            var guess = readLine()?.toInt() ?: 0
+        var didPlayerWin = false
+        var points = 0
+        while(true) {
+            didPlayerWin = false
+            this.guesses = mutableListOf()
+            this.ans = (this.min..this.max).random()
+            println("\nCurrent Score: ${this.score}")
+            println("Guess a number between ${this.min} and ${this.max}!")
+            while (this.guesses.size < this.guessCount) {
+                points = this.guessCount - this.guesses.size
+                println("You have $points guesses left.")
+                print("Write something:\n> ")
+                var guess = readLine()?.toInt() ?: 0
 
-            if (guess > this.ans) {
-                println("Your guess is too high!\n")
+                if (guess > this.ans) {
+                    println("Your guess is too high!\n")
+                }
+                else if (guess < this.ans) {
+                    println("Your guess is too low!\n")
+                }
+                else if (guess == this.ans) {
+                    println("That's correct!")
+                    didPlayerWin = true
+                    break
+                }
+                else {
+                    // something is wrong if you got here
+                }
+
+                this.guesses.add(guess)
             }
-            else if (guess < this.ans) {
-                println("Your guess is too low!\n")
+
+            println("The answer was: ${this.ans}")
+            if (didPlayerWin) {
+                println("You won $points points!")
+                this.score += points
+            } else {
+                println("You lost a point...")
+                this.score -= 1
             }
-            else if (guess == this.ans) {
-                println("That's correct!")
+
+            print("Would you like to continue? (Y/N)\n> ")
+            if (readLine()?.lowercase() ?: "" == "y") {
+                continue
+            } else {
                 break
             }
-            else {
-                // something is wrong if you got here
-            }
 
-            this.guesses.add(guess)
         }
 
-        println("The answer was: ${this.ans}")
+        println("Thank you for playing! Your final score was ${this.score}.")
     }
 
 
