9ccb18ba27062d02df8b7e7a71c214409821a481
Kyle Nishimuta <40375387+knish502@users.noreply.github.com>
2025-03-28 13:04:19 -0500

    Update weatherCli/Main.kt

--- weatherCli/Main.kt
+++ weatherCli/Main.kt
@@ -1,3 +1,36 @@
+import java.net.URL
+import java.net.HttpURLConnection
+import java.io.InputStreamReader
+import java.io.BufferedReader
+
 fun main(args: Array<String>){
-    println("Hello, world!")
+    val latitude = 34.7304
+    val longitude = -86.5861
+
+    val urlString = "https://api.weather.gov/points/$latitude,$longitude"
+    val connection = URL(urlString).openConnection() as HttpURLConnection
+
+    val token = "single_user_test"
+
+    connection.requestMethod = "GET"
+    connection.setRequestProperty("User-Agent", token)
+
+    val responseCode = connection.getResponseCode()
+    println("HTTP Response Code: $responseCode")
+
+    if (responseCode == HttpURLConnection.HTTP_OK){
+
+        val inputStream = connection.inputStream
+        val reader = BufferedReader(InputStreamReader(inputStream))
+
+        val response = reader.readText()
+        reader.close()
+
+        println("Response:\n$response")
+
+        
+    }
+
+    connection.disconnect()
+
 }
