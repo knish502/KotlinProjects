c05f93b8ca9def4fc90df4b421c9dcb379ef4d7b
knish502 <40375387+knish502@users.noreply.github.com>
2025-03-31 20:46:21 -0500

    add weatherRadar

---
+++ weatherRadar/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import java.net.URL
+import javax.swing.ImageIcon
+import javax.imageio.ImageIO
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.SwingUtilities
+
+fun main(args: Array<String>) {
+    val radarUrl = "https://radar.weather.gov/ridge/standard/KHTX_loop.gif"
+
+    val imageIcon = ImageIcon(URL(radarUrl))
+    val label = JLabel(imageIcon)
+
+    val frame = JFrame("NOAA Radar for North Alabama")
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.contentPane.add(label)
+    frame.pack()
+    frame.isVisible = true
+
+
+}
